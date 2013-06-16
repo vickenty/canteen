e3bce7b4b7ee4aab55fdaf3c18f7060562737912
Simona Lazarovici <simona.lazarovici@booking.com>
2013-06-16 18:45:10 +0200

    I've restyled the radio buttons and got the JS to work for them, but somehow now the form won't work anymore.

--- public/buttons.js
+++ public/buttons.js
@@ -1,9 +1,1 @@
-$(".button_container").live("touchstart", 
-	function() {
-      	$('#submit_button').addClass("active");
-      	
-      	}).live("touchend", 
-      		function() {
-	      		$('#submit_button').removeClass("active");
-	      	}
-	      );
+
