9068f68c9337ccf582845b2109ca80f6dc35bb60
Vickenty Fesunov <kent@setattr.net>
2013-06-23 17:36:52 +0200

    Use a set of hidden inputs instead of radiobuttons.

--- public/buttons.js
+++ public/buttons.js
@@ -1,15 +1,13 @@
 $(function() {
 
 	$("#votelist .push_button").bind('click touchstart', function () {
-	
 		// remove active class from all inputs within a meal type 
 		// (go to parent, then identify all labels, remove class 'active' then all radios and deselect all)
 		$(this).parent().find('.push_button').removeClass('active');
-		$(this).parent().find('input:radio').removeAttr('checked');
+		$(this).parent().find('input').val($(this).data('vote'));
 
 		// add 'active' class and select input on current element
 		$(this).addClass('active');
-		$(this).find('input:radio').attr('checked','checked');
 	});
 
 	
